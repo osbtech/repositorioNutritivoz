68e6c652f1a054e7e0ddccb68ea99199631bfa4c
Pablo Flores (pflores2@gmail.com) <Usuario@Notebook>
2016-11-29 21:34:20 -0200

    cambios visuales listado.php

--- nutritivoz/application/views/includes/header.php
+++ nutritivoz/application/views/includes/header.php
@@ -6,16 +6,15 @@
         <!--middle-header-->
         
         <div class="middle-header" id="inicio">
-             <!--center-->
              
-<!--            <div class="center"> -->
-                <div class="middle-header-left"> </div>
+
+            
+            <div class="middle-header-left"> </div>
                 <div class="middle-header-content">
                     <div class="middle-header-img"><a href="http://nutritivoz.com"><img src="<?= asset_url(); ?>img/nutritivoz-header-title.svg" alt="" /></a></div>
                     <div class="middle-header-title">Alimentaci&oacute;n saludable para todos</div>
                 </div>
                 <div class="middle-header-right"> </div>
-       <!--     </div> <!--center-->
         </div><!--end middle-header-->
 
 
