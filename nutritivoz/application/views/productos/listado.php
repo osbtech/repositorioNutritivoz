506043e55f1316c9af5154574cf2a6bc2f88d3e5
osbtech <i.cardarello@osbtech.uy>
2016-12-01 10:47:02 -0300

    Se agrego concepto de zonas

--- nutritivoz/application/views/productos/listado.php
+++ nutritivoz/application/views/productos/listado.php
@@ -7,20 +7,18 @@
         <div class="col-md-8">
             <div class="center">
                 <div class="pricelist-box">
-                    <h2>Lista de precios v&aacute;lida hasta el <?= $fechaCierrePedidos ?><br/></h2>
-                    <p><strong>Env&iacute;os sin costo a todo Montevideo y la Costa de Oro.</strong> Hac&eacute; tu pedido escribiendo a <a href="mailto:ventas@nutritivoz.com">ventas@nutritivoz.com</a></p><p>&nbsp;</p>
                     <?php
                     foreach ($categorias as $categoria) {
                         ?>
                         <table class='table table-striped table-condensed'>
-                            <thead class='thead-inverse'><tr><th><?= $categoria['nombre'] ?></th><th>Productor</th><th>Unidad</th><th>Precio ($)</th><th align='right'>Pedido</th></tr></thead>
+                            <thead ><tr class='head-lista-precios'><th><?= $categoria['nombre'] ?> <img src="<?= asset_url(); ?>img/cat-<?= $categoria['idCategoria'] ?>.png"/></th><th class="text-center">Productor</th><th class="text-center">Unidad</th><th class="text-center">Precio</th><th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pedido</th></tr></thead>
                             <tbody>
                                 <?php
                                 foreach ($categoria['productos'] as $productos) {
                                     foreach ($productos as $producto) {
                                         // echo $producto['nombre'];
                                         ?>
-                                        <tr><td scope='row'><?= $producto['nombre'] ?></td><td><?= $producto['marca'] ?></td><td><?= $producto['unidad'] ?></td><td><?= $producto['precio'] ?></td><td><input id="<?= $producto['idProducto'] ?>" data-idProducto="<?= $producto['idProducto'] ?>" class="form-control myspin rinput" type="number" value="" min="0" max="10" /></td></tr>
+                                        <tr><td scope='row'><?= $producto['nombre'] ?></td><td class="text-center"><?= $producto['marca'] ?></td><td class="text-center"><?= $producto['unidad'] ?></td><td class="text-center"><?= $producto['precio'] ?></td><td><input id="<?= $producto['idProducto'] ?>" data-idProducto="<?= $producto['idProducto'] ?>" class="form-control myspin rinput ingreso-cantidad" type="number" value="" min="0" max="10" /></td></tr>
                                         <?php
                                     }
                                 }
@@ -153,7 +151,7 @@
 
                 <div class="row">
                     <div class="col-lg-12">
-                        <button type="submit" id="btnEnviarPedido" class="btn btn-success btn-md btn-block">Hacer pedido</button>
+                        <button type="submit" id="btnEnviarPedido" class="btn boton-confirmar btn-md btn-block">Hacer pedido</button>
                     </div>	
                 </div>
                 </form> 
@@ -274,7 +272,115 @@
 
         obtenerCarrito();
         agregarCambio();
-        /*$('#comprar').click(function(){
-         window.location.replace("<?= base_url_control() ?>/carrito/resumen");
-         });*/
-    </script>
+    };
+    $('#after').bootstrapNumber();
+    $(".myspin").each(function () {
+        $(this).bootstrapNumber($(this).attr("data-idProducto"));
+    });
+
+    /*  var scrollCarrito = function () {
+     var offset = $("#divCarrito").offset();
+     var topPadding = 15;
+     $(window).scroll(function () {
+     if ($("#divCarrito").height() < $(window).height() && $(window).scrollTop() > offset.top) {
+     $("#divCarrito").stop().animate({
+     marginTop: $(window).scrollTop() - offset.top + topPadding
+     });
+     } else {
+     $("#divCarrito").stop().animate({
+     marginTop: 0
+     });
+     }
+     ;
+     });
+     };*/
+
+    obtenerCarrito();
+    agregarCambio();
+    /*$('#comprar').click(function(){
+     window.location.replace("<?= base_url_control() ?>/carrito/resumen");
+     });*/
+    
+    var lastScrollTop = 0;
+    var indice = 1;
+    $(window).on("scroll", function() {
+        
+        var headerHeight = $('#inicio')[0].clientHeight;
+        var carritoHeight = $("#divCarrito")[0].clientHeight;
+        var carritoTop = $("#divCarrito")[0].offsetTop;
+        var footerHeight = $('footer')[0].clientHeight;
+       
+        console.log("---");
+        console.log("window.innerHeight " + window.innerHeight);
+        console.log("carritoHeight ", carritoHeight);
+        console.log("headerHeight ", headerHeight);
+        console.log(window.innerHeight - carritoHeight - headerHeight);
+        console.log(carritoHeight - document.body.scrollTop);
+        console.log("document.body.scrollTop ", document.body.scrollTop);
+        console.log("document.body.scrollHeight ", document.body.scrollHeight);
+        console.log("carritoTop " + carritoTop);
+        
+           
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            // downscroll code
+            indice = 1;
+           
+            if (document.body.scrollHeight - document.body.scrollTop <= window.innerHeight + 50){
+                $("#divCarrito").css("margin-top", window.innerHeight-carritoHeight-footerHeight - 30);
+                $("#divCarrito").css("position", "fixed");
+             }
+            //else if(carritoHeight - document.body.scrollTop < 450) {
+            else if(Math.abs(window.innerHeight - carritoHeight - headerHeight) < document.body.scrollTop) {  
+                $("#divCarrito").css("margin-top", window.innerHeight-carritoHeight - 10); //inicial -400
+                $("#divCarrito").css("position", "fixed");
+                
+            } else {
+                $("#divCarrito").css("margin-top", headerHeight + 30);
+                $("#divCarrito").css("position", "absolute");
+//                indice = indice + 30;
+//                var a = carritoHeight - 10;
+//                $("#divCarrito").css("margin-top", a + indice);
+//                $("#divCarrito").css("position", "fixed");
+            }
+        } else {
+           // upscroll code
+                     
+            if (document.body.scrollTop <= headerHeight) {
+                $("#divCarrito").css("margin-top", headerHeight + 30);
+                $("#divCarrito").css("position", "absolute");
+            }
+            else if (carritoTop >= 0){
+                $("#divCarrito").css("margin-top", 10);
+                $("#divCarrito").css("position", "fixed");
+            }
+            else {
+                indice = indice + 30;
+                var a = window.innerHeight-carritoHeight - 10;
+                $("#divCarrito").css("margin-top", a + indice);
+                $("#divCarrito").css("position", "fixed");
+            }
+
+           
+        }
+        lastScrollTop = st;  
+
+    });
+        
+        
+        
+</script>
+
+<style>
+    #divCarrito{
+    width: 25%;
+    float:right;
+    margin: 20px;
+    margin-top:270px;
+    float:right;
+    right:0;
+    top:0;
+    margin-right: 4%;
+    position: absolute;
+}
+</style>
