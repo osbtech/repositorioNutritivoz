f876cc0555d8fd633241cdaba8b0bad0663ecbfb
osbtech <i.cardarello@osbtech.uy>
2016-12-01 10:46:45 -0300

    Se agrego concepto de zonas

--- nutritivoz/application/views/includes/header.php
+++ nutritivoz/application/views/includes/header.php
@@ -1,22 +1,31 @@
 <!--body-->
-    <body>
+<body>
 
-        <!--header-->
+    <!--middle-header-->
 
-        <!--middle-header-->
-        
-        <div class="middle-header" id="inicio">
-             <!--center-->
-             
-<!--            <div class="center"> -->
-                <div class="middle-header-left"> </div>
-                <div class="middle-header-content">
-                    <div class="middle-header-img"><a href="http://nutritivoz.com"><img src="<?= asset_url(); ?>img/nutritivoz-header-title.svg" alt="" /></a></div>
-                    <div class="middle-header-title">Alimentaci&oacute;n saludable para todos</div>
+    <div class="middle-header" id="inicio">
+
+        <div class="row">
+            <div class="container">
+                <div class="col-md-4">
+                    <p> <span class="header-text-1">Envíos a Montevideo y Ciudad de la Costa</span><BR/>
+                        <span class="header-text-2">Gratis para compras mayores a $ 1000</span>
+                    <BR/><BR/>
+                        <span class="header-text-3">Precios vigentes hasta el</span><BR/>
+                        <span class="header-text-2">Miércoles 16/11/2016</span></p>
                 </div>
-                <div class="middle-header-right"> </div>
-       <!--     </div> <!--center-->
-        </div><!--end middle-header-->
+                <div class="col-md-4">
+                    <div class="middle-header-content">
+                        <div class="middle-header-img"><a href="http://nutritivoz.com"><img src="<?= asset_url(); ?>img/nutritivoz-header-title.svg" alt="" /></a></div>
+                        <div class="middle-header-title">Alimentaci&oacute;n saludable para todos</div>
+                    </div>
+
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+
+    </div>
 
 
-		        <div  class="contact">
+                    <div  class="contact">
