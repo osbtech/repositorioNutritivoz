b1d822099f5d229212ecfe8dda8e902653546896
Pablo Flores (pflores2@gmail.com) <Usuario@Notebook>
2016-12-06 17:34:38 -0200

    c

--- nutritivoz/application/views/includes/header.php
+++ nutritivoz/application/views/includes/header.php
@@ -8,20 +8,20 @@
         <div class="row">
             <div class="container">
                 <div class="col-md-4">
+                    <div class="middle-header-content">
+                        <div class="middle-header-img"><a href="http://nutritivoz.com"><img src="<?= asset_url(); ?>img/nutritivoz-header-title.svg" alt="" /></a></div>
+                        <div class="middle-header-title">Alimentaci&oacute;n saludable para todos</div>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
                     <p> <span class="header-text-1">Envíos a Montevideo y Ciudad de la Costa</span><BR/>
                         <span class="header-text-2">Gratis para compras mayores a $ 1000</span>
                     <BR/><BR/>
                         <span class="header-text-3">Precios vigentes hasta el</span><BR/>
                         <span class="header-text-2">Miércoles 16/11/2016</span></p>
                 </div>
-                <div class="col-md-4">
-                    <div class="middle-header-content">
-                        <div class="middle-header-img"><a href="http://nutritivoz.com"><img src="<?= asset_url(); ?>img/nutritivoz-header-title.svg" alt="" /></a></div>
-                        <div class="middle-header-title">Alimentaci&oacute;n saludable para todos</div>
-                    </div>
 
-                </div>
-                <div class="col-md-4"></div>
             </div>
         </div>
 
