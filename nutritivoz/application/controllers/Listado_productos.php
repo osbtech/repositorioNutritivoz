791e7fd401951164eb55ff163551e3c54ab1e43e
Juan Pablo <Juan Pablo@DESKTOP-I3PTOOG>
2016-12-15 20:05:02 -0300

    JPSALAZAR

--- nutritivoz/application/controllers/Listado_productos.php
+++ nutritivoz/application/controllers/Listado_productos.php
@@ -28,6 +28,7 @@ class Listado_productos extends CI_Controller {
         $this->load->view('includes/footer');
     }
 
+
     public function listado_productos() {
         $this->session->sess_expiration = '28800';      // Session expires in 8 hours
         $this->load->helper('form');
@@ -41,52 +42,7 @@ class Listado_productos extends CI_Controller {
         $this->form_validation->set_rules('esquina2', 'Esquina 2', 'required');
         //$this->form_validation->set_rules('aclDireccion', 'Aclaración dirreción', 'required');
         $this->form_validation->set_rules('localidad', 'Localidad', 'required');
-        // $this->form_validation->set_rules('horario', 'Horario', 'required');
-
-        /* login con facebook */
-        $this->load->library('facebook'); // Automatically picks appId and secret from config
-        $user = $this->facebook->getUser();
-        if ($user) {
-            try {
-                //$data['user_profile'] = $this->facebook->api('/me?fields=name,email');
-                $us = $this->facebook->api('/me?fields=name,email');
-                $cliente = $this->clientes_model->obtener_clienteByMail($us['email']);
-                if ($cliente == null) {
-                    $idCliente = $this->clientes_model->guardar_cliente($us['email'], $us['name'], '', $us['id'], '0', '0', '', '', '', '', '');
-                    $data = array(
-                        'username' => $us['name'],
-                        'email' => $us['email'],
-                        'idUsuario' => $idCliente,
-                        'fbId' => $us['id']
-                    );
-                    $this->session->set_userdata($data);
-                    //  redirect('listado_productos/listado_productos');
-                } else {
-                    $this->clientes_model->actualizar_cliente($us['email'], $us['name'], $cliente['celular'], $us['id'], $cliente['idZona'], $cliente['idLocalidad'], $cliente['direccion'], $cliente['direccion_aclaracion'], $cliente['esquina1'], $cliente['esquina2']);
-                    $data = array(
-                        'username' => $cliente['nombre'],
-                        'email' => $cliente['correo'],
-                        'idUsuario' => $cliente['idCliente'],
-                        'fbId' => $cliente['fbId']
-                    );
-                    $this->session->set_userdata($data);
-                    //  redirect('listado_productos/listado_productos');
-                }
-            } catch (FacebookApiException $e) {
-                $user = null;
-            }
-        } else {
-            
-        }
-        if ($user) {
-            $data['logout_url'] = site_url('listado_productos/logoutFB'); // Logs off application
-        } else {
-            $data['login_url'] = $this->facebook->getLoginUrl(array(
-                'redirect_uri' => site_url('listado_productos/listado_productos'),
-                'scope' => array('email') // permissions here
-            ));
-        }
-        /* fin login con facebook */
+        // $this->form_validation->set_rules('horario', 'Horario', 'required');        
 
         if ($this->form_validation->run() === FALSE) {
             //Obtener cliente
@@ -185,8 +141,25 @@ class Listado_productos extends CI_Controller {
         }
     }
 
+ public function loginAjax() {
+        $usuario = $this->clientes_model->login_usuarios($this->input->post('correo'), $this->input->post('contrasena'));
+        if ($usuario == null) {
+            $ret['resultado'] = false;                
+        } else {
+            $data = array(
+                'username' => $usuario['nombre'],
+                'email' => $usuario['correo'],
+                'idUsuario' => $usuario['idCliente'],
+                'fbId' => $usuario['fbId']
+            );
+            $this->session->set_userdata($data);
+            $ret['resultado'] = true;
+        }
+        echo json_encode($ret);
+    }
+
     public function logout() {
-        $this->session->unset_userdata(array('username', 'email', 'idUsuario','zona'));
+        $this->session->unset_userdata(array('username', 'email', 'idUsuario','zona','log1','fbId'));
         $this->logoutFB();
         redirect('listado_productos/listado_productos');
         //poner lo mismo en logout facebook
--- nutritivoz/application/controllers/Login.php
+++ nutritivoz/application/controllers/Login.php
@@ -63,4 +63,55 @@ class Login extends CI_Controller {
         redirect('welcome/login');
     }
 
+
+
+public function loginFacebook()
+{
+ /* login con facebook */
+        $this->load->library('facebook'); // Automatically picks appId and secret from config
+        $user = $this->facebook->getUser();
+        if ($user) {
+            try {
+                //$data['user_profile'] = $this->facebook->api('/me?fields=name,email');
+                $us = $this->facebook->api('/me?fields=name,email');
+                $cliente = $this->clientes_model->obtener_clienteByMail($us['email']);
+                if ($cliente == null) {
+                    $idCliente = $this->clientes_model->guardar_cliente($us['email'], $us['name'], '', $us['id'], '0', '0', '', '', '', '', '');
+                    $data = array(
+                        'username' => $us['name'],
+                        'email' => $us['email'],
+                        'idUsuario' => $idCliente,
+                        'fbId' => $us['id']
+                    );
+                    $this->session->set_userdata($data);
+                    //  redirect('listado_productos/listado_productos');
+                } else {
+                    $this->clientes_model->actualizar_cliente($us['email'], $us['name'], $cliente['celular'], $us['id'], $cliente['idZona'], $cliente['idLocalidad'], $cliente['direccion'], $cliente['direccion_aclaracion'], $cliente['esquina1'], $cliente['esquina2']);
+                    $data = array(
+                        'username' => $cliente['nombre'],
+                        'email' => $cliente['correo'],
+                        'idUsuario' => $cliente['idCliente'],
+                        'fbId' => $cliente['fbId']
+                    );
+                    $this->session->set_userdata($data);
+                    //  redirect('listado_productos/listado_productos');
+                }
+            } catch (FacebookApiException $e) {
+                $user = null;
+            }
+        } else {
+            
+        }
+        if ($user) {
+            $data['logout_url'] = site_url('listado_productos/logoutFB'); // Logs off application
+        } else {
+            $data['login_url'] = $this->facebook->getLoginUrl(array(
+                'redirect_uri' => site_url('listado_productos/listado_productos'),
+                'scope' => array('email') // permissions here
+            ));
+        }
+        /* fin login con facebook */
+}
+
+
 }
